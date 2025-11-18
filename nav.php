9498e320fcbb88b2948f5bbff1fae971e5932a33
Igor Aleksejev <igoralekseje@gmail.com>
2025-11-18 11:05:33 +0200

    -js 1.töö muusikaküsitlus on lisatud

--- nav.php
+++ nav.php
@@ -2,7 +2,17 @@
     <ul>
         <li><strong><a href="?link=avaleht.php">Avaleht</a></strong></li>
         <li><strong><a href="?link=gitKasutamine.php">Git käsud</a></strong></li>
-        <li><strong><a href="?link=too1.php">Töö 1</a></strong></li>
-        <li><strong><a href="https://igoraleksejev24.thkit.ee/">Vana index</a></strong></li>
+        <li>
+            <strong>
+                <a href="?link=muusikaKysitlus.php">
+                    JS: Muusika küsitlus
+                </a>
+            </strong>
+        </li>
+
+        <li><strong>
+                <a href="https://igoraleksejev24.thkit.ee/" target="_blank">
+                    Vana index
+                </a></strong></li>
     </ul>
 </nav>
