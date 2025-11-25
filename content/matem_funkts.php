70ee4da88204b7549f9bce9bfe94ddfca8db4c39
Igor Aleksejev <igoralekseje@gmail.com>
2025-11-25 09:47:55 +0200

    matematiilesed tehed ja funktsioonid on proovitud

--- content/matem_funkts.php
+++ content/matem_funkts.php
@@ -1,1 +1,76 @@
 <?php
+echo "<h2>Matemaatilised tehted</h2>";
+$arv1= 1.21;
+$arv2= 2.54;
+
+echo "Esimene arv on .$arv1, teine arv on .$arv2 ";
+echo "<br>";
+echo "+ Liitmise tulemus: ".($arv1+$arv2);
+echo "<br>";
+echo "- Lahutamise tulemus: ".($arv1-$arv2);
+echo "<br>";
+echo "* Korrutise tulemus: ".($arv1*$arv2);
+echo "<br>";
+echo "/ Jagatise tulemus: ".($arv1/$arv2);
+echo "<br>";
+echo "<h3>Matemaatilised funktsioonid</h3>";
+echo "Väiksem arv: ";
+echo min($arv1,$arv2);
+echo "<br>";
+echo "Suurem arv: ";
+echo max($arv1,$arv2);
+echo "<br>";
+echo "round(arv) - Ümardamine täisarvani: ";
+echo round($arv1);
+echo "<br>";
+echo "ceil(arv) - Ümardab jargmise arvuni: ";
+echo ceil($arv1);
+echo "<br>";
+echo "floor(arv) - Ümardab allapoole täisarvani: ";
+echo floor($arv1);
+echo "<br>";
+echo "round(arv, 1) - Ümardab ühe komma kohtani: ";
+echo round($arv1, 1 );
+echo "<br>";
+
+echo rand(1,100);
+echo " - juhuslik arv";
+echo "<br>";
+echo "pow(arv,2) - astendamine: ";
+echo pow($arv1,2);
+echo "<br>";
+echo"sqrt(arv) - ruutjuur: ";
+echo sqrt($arv2);
+echo "<br>";
+echo pi();
+echo "<br>";
+
+echo "<h2>Omistamise operaatorid</h2>";
+$x = 10;
+$y = 20;
+
+echo "Suurendamine ühe võrra: ";
+$x ++; // $x = $x+1
+echo $x;
+echo "<br>";
+echo "Vahendamine ühevõrra: ";
+$x --;
+echo $x;
+echo "<br>";
+$x*=$y;// x= x*y
+echo $x;
+echo "<br>";
+
+$x/=$y;// x= x/y
+echo $x;
+
+echo "<br>";
+$nimi = 'igor';
+$perenimi = 'aleksejev';
+//$nimi .= $perenimi;
+echo $nimi;
+echo "<br>";
+$format = 'Tere, %s %s, arv x: %d';
+printf($format, $perenimi, $nimi,$x);
+
+echo "<br>";
--- nav.php
+++ nav.php
@@ -9,6 +9,27 @@
                 </a>
             </strong>
         </li>
+        <li>
+            <strong>
+                <a href="?link=matem_funkts.php">
+                    Matemaatika funktsioonid
+                </a>
+            </strong>
+        </li>
+        <li>
+            <strong>
+                <a href="?link=tekstfunktsioonid.php">
+                    Tekst funktsioonid
+                </a>
+            </strong>
+        </li>
+        <li>
+            <strong>
+                <a href="?link=ajafunktsioonid.php">
+                    Aja funktsioonid
+                </a>
+            </strong>
+        </li>
 
         <li><strong>
                 <a href="https://igoraleksejev24.thkit.ee/" target="_blank">
